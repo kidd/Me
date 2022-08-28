91e3cde6cd2619558d5bb5e2b66fda83f04e54bf
Raimon Grau <raimonster@gmail.com>
2022-08-28 22:22:13 +0100

    cv update

--- cv/shell.nix
+++ cv/shell.nix
@@ -6,13 +6,4 @@ stdenv.mkDerivation {
   rubber
   tetex
   ];
-
-  shellHook = ''
-    export PATH="$PWD/node_modules/.bin/:$PATH"
-    alias scripts='jq -r ".scripts" package.json'
-    function nmod() {
-      for i in {mo,co,s}; do nest g $i $1; done
-      yarn lint --fix
-    }
-'';
 }
